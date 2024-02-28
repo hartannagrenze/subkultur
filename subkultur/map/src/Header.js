5db171ea8ffdfa68a79b49da2846d35a19892d3d
hartannagrenze <91656529+hartannagrenze@users.noreply.github.com>
2024-02-28 15:15:27 +0100

    small changes

---
+++ subkultur/map/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="/favicon.png" alt="Logo" />
+      {/* Weitere Header-Elemente können hier hinzugefügt werden */}
+    </header>
+  );
+}
